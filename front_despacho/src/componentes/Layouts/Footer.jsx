3d1632a8a6a31c503c36c2c134666acd2d52e12e
clmoralesl <cl.moralesl@duocuc.cl>
2026-05-28 21:23:28 -0400

    Prueba de deploy fontend

--- front_despacho/src/componentes/Layouts/Footer.jsx
+++ front_despacho/src/componentes/Layouts/Footer.jsx
@@ -74,7 +74,7 @@ function Footer() {
             <a href="#" className="hover:underline">
               ITPCARGO™
             </a>
-            . Todos los derechos reservados.
+            . Derechos reservados.
           </span>
         </div>
       </div>
